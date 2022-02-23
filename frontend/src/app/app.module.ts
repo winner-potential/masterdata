d5a6206c388039f598e94957d9ac0133f87a4796
Peter Conrad <peter.conrad@uni-jena.de>
2022-02-23 17:08:43 +0100

    (frontend) Update to 12, disable ngx-sortablejs
    With the update of Angular core to version 12, this commit temporarily
    suspends usage of ngx-sortablejs, of which currently no compatible
    version is available. Installing it would require us to either accept
    legacy peer dependencies (potentially leading to issues), using an
    untrusted third-party fork, or maintaining one ourselves. As
    ngx-sortablejs currently only plays a minor role in the project and no
    major inconvenience is expected, we choose to simply not use it for now
    and reactivate it once a compatible version becomes available.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import {SortablejsModule} from "ngx-sortablejs";
+//import {SortablejsModule} from "ngx-sortablejs";
 
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
@@ -211,7 +211,7 @@ const appRoutes: Routes = [
     MatTreeModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    SortablejsModule.forRoot({ animation: 150 })
+    //SortablejsModule.forRoot({ animation: 150 })
   ],
   providers: [
     {
